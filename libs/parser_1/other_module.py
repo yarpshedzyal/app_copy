57f5e93426256f777688a56d1ef39338eac9307a
yar.pshe <yar.pshe@gmail.com>
2024-03-22 19:53:32 +0200

    hotfix2

--- libs/parser_1/other_module.py
+++ libs/parser_1/other_module.py
@@ -100,8 +100,8 @@ def parser_solo(url):
         # if product_from_line in soup.get_text():
         #     price_element = soup.select_one('#priceBox > div.pricing > p > span')
 
-        p_r_error_element = soup.select_one('#priceBox > div.pricing > div > p.leading-none.mb-0 > span')
-        if p_r_error_element:
+        p_r_error_text = 'Contact us or login for pricing'
+        if p_r_error_text in soup.get_text():
             price_element = soup.select_one('#priceBox > div.pricing > div > p.leading-none.mb-0 > span')
             if price_element:
                 price = price_element.text.strip().replace("$", "").replace(",", "")
