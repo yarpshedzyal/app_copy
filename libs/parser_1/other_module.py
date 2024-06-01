0649d2edcaabb61cb74d26468724a95efd502405
yar.pshe <yar.pshe@gmail.com>
2024-06-01 16:22:38 +0300

    test

--- libs/parser_1/other_module.py
+++ libs/parser_1/other_module.py
@@ -102,7 +102,7 @@ def parser_solo(url):
         #     price_element = soup.select_one('#priceBox > div.pricing > p > span')
 
         p_r_error_text = 'Contact us or '
-        if p_r_error_text in soup.get_text():
+        if p_r_error_text in soup.get_text() and not table_element:
             price_element = soup.select_one('#priceBox > div.pricing > div > p.leading-none.mb-0')
             if price_element:
                 price = price_element.text.strip().replace("$", "").replace(",", "")
